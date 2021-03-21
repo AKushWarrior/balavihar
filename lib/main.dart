1c09f5e884b0500c40d3a6ac6dbb13e94c0a604b
Mohan Kishore <mohanki@amazon.com>
2021-03-21 11:13:07 -0700

    that option no longer exists, whoops

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class BalviharApp extends StatelessWidget {
   Widget build(BuildContext context) {
     Nav.init();
     return MaterialApp(
-      debugShowCheckModeBanner: false,
       title: 'Balvihar Demo',
       theme: ThemeData.light().copyWith(
         textTheme: GoogleFonts.oswaldTextTheme().copyWith(
